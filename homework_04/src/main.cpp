2ced16de24c9ff6943c09d83a52b4abd2f9df55d
Constantine-Kotyk <c.kotik@yahoo.com>
2026-04-28 21:13:53 +0000

    HW04 add constants, create structure, file io

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -1,4 +1,13 @@
+#include <fstream>
 #include <iostream>
+
+const int TICKS_PER_REVOLUTION    = 1024;
+const double WHEEL_RADIUS_M       = 0.3;
+const double WHEELBASE_M          = 1.0;
+
+struct odometry_data {
+    long timestamp_ms, fl_ticks, fr_ticks, bl_ticks, br_ticks;
+};
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -6,17 +15,25 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // TODO: implement wheel odometry for a 4-wheel differential-drive UGV.
-    //
-    // Parameters:
-    //   ticks_per_revolution = 1024
-    //   wheel_radius_m       = 0.3
-    //   wheelbase_m          = 1.0
-    //
     // Input:  text file with 5 whitespace-separated numbers per line:
     //         timestamp_ms fl_ticks fr_ticks bl_ticks br_ticks
+    std::ifstream in(argv[1]);
+    if (!in.is_open()) {
+        std::cerr << "failed to read data from file: " << argv[1] << "\n";
+        return 1;
+    }
+
+    odometry_data data[2];
+    in >> data[0].timestamp_ms >> data[0].fl_ticks >> data[0].fr_ticks >> data[0].bl_ticks >> data[0].br_ticks;
+
+    std::cout << data[0].timestamp_ms << data[0].fl_ticks << data[0].fr_ticks << data[0].bl_ticks << data[0].br_ticks;
+
+    // TODO: implement wheel odometry for a 4-wheel differential-drive UGV.
     // Output: same tabular format on stdout, starting from the second sample:
     //         timestamp_ms x y theta
 
+    in.close();
+
     return 0;
 }
+
